1bda52355d9b6a4a2a92cd06e3f85f607cbfd2bd
ThiagoPScc <141779315+ThiagoPScc@users.noreply.github.com>
2025-01-06 21:47:59 -0300

    update

--- Aulafuncao.js
+++ Aulafuncao.js
@@ -1,3 +1,4 @@
+/*
 function falaOi(){
     console.log('Oi');
 }
@@ -15,3 +16,34 @@ const arrowf = () => {
     console.log('Função arrow, diferente na palabra this')
 }
 arrowf();
+*/
+/*
+//Funções de callback
+//são usadas para quando não sabemos quanto tempo vai demorar para cada ação acontecer e precisamos fazer o código seguir uma ordem.
+//São funções que são iniciadas somente quando uma ação é terminada
+function f1(callback){
+    setTimeout(function(){
+        console.log('f1');
+        if (callback) callback();
+    },1500)
+   
+}
+function f2(callback){
+    setTimeout(function(){
+        console.log('f2');
+        if (callback) callback();
+    },1000)
+}
+function f3(callback){
+    setTimeout(function(){
+        console.log('f3');
+        if (callback) callback();
+    },500)
+}
+
+f1(function(){
+    f2(function(){
+        f3();
+    });
+});
+*/
